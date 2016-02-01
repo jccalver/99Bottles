694f4908ec758b0ee65558c96abd4272f7c2157c
Jon <jccalver@oakland.edu>
2016-02-01 11:16:57 -0500

    added in the main class a function call to Ninety_Nine_Bottles_Of_Beer(99), and a parameter for that funciton

--- BeerSong.java
+++ BeerSong.java
@@ -2,13 +2,14 @@ class BeerSong
 {
 	public static void main(String[] args)
 	{
-
+		
+		Ninety_Nine_Bottles_Of_Beer( 99 ); 	
+	
 	}
 
 	
-	public static void Ninety_Nine_Bottles_Of_Beer()
+	public static void Ninety_Nine_Bottles_Of_Beer(int numBottles)
 	{
 
         }
-    }
         
